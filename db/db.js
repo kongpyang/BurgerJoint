28b64f5e9eb856347b038fe53e7a0575e6a60174
kyangp@gmail.com <kongpyang@Kongs-MacBook-Pro.local>
2019-09-27 00:57:57 -0500

    color scheme

--- db/db.js
+++ db/db.js
@@ -1,10 +1,6 @@
 var Sequelize = require('sequelize');
 
-<<<<<<< HEAD
-var sequelize = new Sequelize("burgersdb", "root", "root", {
-=======
 var sequelize = new Sequelize("burgers_db", process.env.DB_USER, process.env.PASS, {
->>>>>>> 5271fbd4d1a60cfd5c2fddc9d59c3978a32ace1e
     dialect: "mysql",
     host: process.env.DB_HOST,
     port: process.env.DB_PORT
